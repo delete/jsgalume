de69e4af314c76535589653be48e1298a29bdf68
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2016-02-12 16:41:01 -0200

    API returns promise now
    Return promise make it easier to test.

--- index.js
+++ index.js
@@ -15,15 +15,50 @@ var api = new API();
 
 if ( argv.a && argv.m ) {
 
-  api.getLyrics(argv.a, argv.m);
+  api.getLyrics(argv.a, argv.m).then(
+    function (data) {
+      console.log('\n\n');
+      console.log('- Artista: ' + data.artist);
+      console.log('- Música: ' + data.music);
+      console.log('- URL: ' + data.music_url);
+      console.log('\n\n -ORIGINAL:\n' + data.text);
+      if (data.translate) {
+        console.log('\n\n -TRANSLATE : ' + data.translate);
+      }
+    },
+    function (err) {
+      console.log(err);
+    }
+  );
 
 } else if ( argv.a && argv.d) {
 
-  api.getDiscography(argv.a);
+  api.getDiscography(argv.a).then(
+    function (data) {
+      for (var item of data)
+        console.log(item.desc + ' - ' + item.published);
+    },
+    function (err) {
+      console.log(err);
+    }
+  );
 
 } else if ( argv.a && argv.b ) {
   
-  api.getSongs(argv.a, argv.b);
+  api.getSongs(argv.a, argv.b).then(
+    function (data) {
+      // Remove the last char, because is a "/"
+      const URL = api.MAIN_URL.slice(api.API_URL, -1)
+
+      for (var item of data) {
+        console.log(item.desc);
+        console.log(URL + item.url + '\n');
+      }
+    },
+    function (err) {
+      console.log(err);
+    }
+  );
 
 } else {
   console.log('Something went wrong, see --help for more information.');
